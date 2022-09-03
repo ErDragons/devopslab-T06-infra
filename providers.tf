bfc596a9c40620b31168b1133315792d4d991c59
Matheus Henrique da Silva <64704184+ErDragons@users.noreply.github.com>
2022-09-02 23:17:51 -0300

    Fix Credentials

--- providers.tf
+++ providers.tf
@@ -10,8 +10,6 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/home/matheushenriquesilva31/gcpkey.json")
-
   project = "lab-devops-cloud-361318"
   region  = "us-east4"
   zone    = "us-east4-c"
